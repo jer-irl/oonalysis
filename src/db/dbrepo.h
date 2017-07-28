be0081d69e77f3a4b7443b7f7e732471345d9549
Jeremy Schroeder <jpschroeder2014@gmail.com>
2017-07-27 20:34:07 -0400

    Added adding functions

--- src/db/dbrepo.h
+++ src/db/dbrepo.h
@@ -1,13 +1,13 @@
 #include "dbtypes.h"
 
 bool add_dbfile(db_file dbfile);
-db_file get_dbfile(int id);
+bool get_dbfile(int id, db_file* dbfile);
 int get_dbfiles(db_file** files);
 bool update_dbfile(db_file dbfile, int id);
 bool delete_dbfile(int id);
 
 bool add_dbcppinclusion(db_cppinclusion inclusion);
-db_cppinclusion get_dbcppinclusion(int id);
+bool get_dbcppinclusion(int id, db_cppinclusion* incl);
 int get_dbcppinclusions(db_cppinclusion** inclusions);
 bool update_dbcppinclusion(db_cppinclusion inclusion, int id);
 bool delete_dbcppinclusion(int id);
