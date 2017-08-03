076510f8de0114569cfada21db097190204af7f6
Jeremy Schroeder <jpschroeder2014@gmail.com>
2017-08-03 18:42:04 -0400

    Added filetype dispatching

--- src/core/parse.cc
+++ src/core/parse.cc
@@ -1,5 +1,7 @@
 #include <stdlib.h>
 #include "parse.h"
+#include "filetype.h"
+#include "clang.h"
 
 
 namespace oonalysis {
@@ -7,7 +9,23 @@ namespace core {
 
 void parse_files(const std::vector<std::string>& files)
 {
+    Lang project_lang = lang_from_filenames(files);
     for (auto file : files) {
+        if (lang_from_filename(file) != project_lang) { continue; }
+
+        switch (project_lang) {
+        case C:
+        case CPP:
+            main_clang(file);
+        case HS:
+        case PY:
+        case UNKNOWN:
+        case OTHER:
+        case NONE:
+        default:
+            continue;
+        }
+
 
 
     }
