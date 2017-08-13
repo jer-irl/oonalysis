7b2b674caba530666cd6bc8887f4df3b33f501f8
Jeremy Schroeder <jpschroeder2014@gmail.com>
2017-08-13 12:53:37 -0400

    Clang hooked up

--- src/core/clang/clang.cc
+++ src/core/clang/clang.cc
@@ -28,36 +28,58 @@ CXChildVisitResult cursor_visitor(CXCursor cur, CXCursor parent, CXClientData cl
     return handler(cur, parent, client_data);
 }
 
-
-void parse_file(const std::string& filename, CXTranslationUnit* tu)
+void parse_translation_unit(CXTranslationUnit tu)
 {
-    LOG(INFO, "Parsing clang file: %s", filename.c_str());
+    LOG(INFO, "Parsing clang file");
 
+    // Get file and add to db
+    CXString cxname = clang_getTranslationUnitSpelling(tu);
     db::db_file file;
     file.id = 0;
-    file.filename = filename;
+    file.filename = clang_getCString(cxname);
     add_dbfile(file);
+    clang_disposeString(cxname);
 
     clang_visitChildren(
-            clang_getTranslationUnitCursor(*tu),
+            clang_getTranslationUnitCursor(tu),
             cursor_visitor,
             0);
 }
 
-void parse_translation_unit(CXTranslationUnit* tu)
+std::vector<CXTranslationUnit> make_translation_units(CXIndex* index, const std::vector<std::string>& files)
 {
-}
+    std::vector<CXTranslationUnit> res;
 
-void make_translation_units(CXIndex* index, const std::vector<std::string>& files)
-{
+    for (auto file : files) {
+        CXTranslationUnit tu;
+        auto err = clang_parseTranslationUnit2(
+                *index,
+                file.c_str(),
+                nullptr,
+                0,
+                nullptr,
+                0,
+                CXTranslationUnit_None,
+                &tu);
+
+        if (err != CXError_Success) {
+            LOG(ERROR, "Error in creating translation unit");
+            exit(1);
+        }
+        res.push_back(tu);
+    }
+    return res;
 }
 
 void main_clang(const std::vector<std::string>& files)
 {
     LOG(INFO, "Entering clang main");
-    //CXIndex index = clang_createIndex(1, 1);
+    CXIndex index = clang_createIndex(1, 1);
+    std::vector<CXTranslationUnit> tus =  make_translation_units(&index, files);
 
-    //CXTranslationUnit tu = clang_createTranslationUnit(index, filename.c_str());
+    for (auto tu : tus) {
+        parse_translation_unit(tu);
+    }
 }
 
 
