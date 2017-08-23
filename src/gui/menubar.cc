67018ddd8e5143b6198e2fc15e395cbf4c8b64e1
Jeremy Schroeder <jpschroeder2014@gmail.com>
2017-08-22 21:08:53 -0400

    Added testing framework and restructured to minimize libraries

--- src/gui/menubar.cc
+++ src/gui/menubar.cc
@@ -8,7 +8,6 @@ MenuBar::MenuBar(int width) : Fl_Menu_Bar(0, 0, width, 20, "oonalysis")
     // Buttons
     this->add("File");
     this->add("Action");
-    //this->show();
 }
 
 } } // namespace oonalysis::gui
