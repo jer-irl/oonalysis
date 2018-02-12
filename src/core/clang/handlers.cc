05fa0a782cd04d2a30e439c6f5ab9ac906fcd26c
Jeremy Schroeder <jpschroeder2014@gmail.com>
2018-02-12 12:20:03 -0600

    Only get function decls from file

--- src/core/clang/handlers.cc
+++ src/core/clang/handlers.cc
@@ -11,10 +11,10 @@ extern "C" {
 namespace orm = sqlite_orm;
 namespace oonalysis::core::clang {
 
-#pragma clang diagnostic push
-#pragma clang diagnostic ignored "-Wall"
-
 CXChildVisitResult handle_inclusion_directive(CXCursor cur, CXCursor parent, CXClientData client_data) {
+    (void) parent;
+    (void) client_data;
+
     LOG(DEBUG, "Handling inclusion directive");
 
     CXFile includee = clang_getIncludedFile(cur);
@@ -24,10 +24,12 @@ CXChildVisitResult handle_inclusion_directive(CXCursor cur, CXCursor parent, CXC
     }
     CXString filename = clang_getFileName(includee);
     std::string includee_name = clang_getCString(filename);
+    clang_disposeString(filename);
 
     auto tu = clang_Cursor_getTranslationUnit(cur);
     CXString filename2 = clang_getTranslationUnitSpelling(tu);
     std::string includer_name = clang_getCString(filename2);
+    clang_disposeString(filename2);
 
     // TODO
     // db::add_new_inclusion(includer_name, includee_name);
@@ -36,12 +38,16 @@ CXChildVisitResult handle_inclusion_directive(CXCursor cur, CXCursor parent, CXC
 }
 
 CXChildVisitResult handle_function_decl(CXCursor cur, CXCursor parent, CXClientData client_data) {
-    auto tu = clang_Cursor_getTranslationUnit(cur);
-    CXString filename = clang_getTranslationUnitSpelling(tu);
-    std::string fname = clang_getCString(filename);
+    (void) parent;
+
+    CXSourceLocation loc = clang_getCursorLocation(cur);
+    if (!clang_Location_isFromMainFile(loc)) {
+        return CXChildVisit_Recurse;
+    }
 
     CXString cxname = clang_getCursorSpelling(cur);
     std::string name = clang_getCString(cxname);
+    clang_disposeString(cxname);
 
     CursorData cd = *(CursorData *) client_data;
 
@@ -54,7 +60,5 @@ CXChildVisitResult handle_function_decl(CXCursor cur, CXCursor parent, CXClientD
 CXChildVisitResult handle_other(CXCursor cur, CXCursor parent, CXClientData client_data) {
     return CXChildVisit_Recurse;
 }
-
-#pragma clang diagnostic pop
 
 } // end namespace oonalysis::core::clang
