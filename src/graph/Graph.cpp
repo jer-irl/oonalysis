a960c7825b36e35ebf842573804fc2d532a530dc
Jeremy Schroeder <jpschroeder2014@gmail.com>
2018-05-07 20:39:21 -0500

    Working on graph lib to replace graphviz

---
+++ src/graph/Graph.cpp
@@ -0,0 +1,41 @@
+#include "Graph.hpp"
+
+namespace oonalysis::graph {
+
+Graph::Graph(std::unordered_set<std::shared_ptr<Node>>& nodes,
+             std::unordered_set<std::shared_ptr<Edge>>& edges)
+        : nodes(nodes), edges(edges) {
+    initializeLayout();
+}
+
+void Graph::initializeLayout() {
+}
+
+void Graph::addEdge(std::shared_ptr<Edge> edge) {
+    edges.insert(edge);
+}
+
+void Graph::addNode(std::shared_ptr<Node> node) {
+    nodes.insert(node);
+}
+
+void Node::addIncomingEdge(std::shared_ptr<Edge> e) {
+    incomingEdges.insert(e);
+}
+
+Node::Node() : position(Point(0, 0)) {
+}
+
+void Node::removeIncomingEdge(std::shared_ptr<Edge> e) {
+    incomingEdges.erase(e);
+}
+
+void Node::addOutgoingEdge(std::shared_ptr<Edge> e) {
+    outgoingEdges.insert(e);
+}
+
+void Node::removeOutgoingEdge(std::shared_ptr<Edge> e) {
+    outgoingEdges.erase(e);
+}
+
+} // namespace oonalysis::graph
---
+++ src/graph/Graph.hpp
@@ -0,0 +1,62 @@
+#ifndef GRAPH_GRAPH_HPP
+#define GRAPH_GRAPH_HPP
+
+#include <unordered_set>
+#include <memory>
+
+namespace oonalysis::graph {
+
+class Node;
+class Edge;
+
+struct Point {
+    Point(float x, float y) : x(x), y(y) { }
+    float x;
+    float y;
+};
+
+class Graph {
+public:
+    Graph(std::unordered_set<std::shared_ptr<Node>>& nodes, std::unordered_set<std::shared_ptr<Edge>>& edges);
+
+    virtual void initializeLayout();
+    virtual void tick() = 0;
+
+    void addNode(std::shared_ptr<Node> node);
+    void addEdge(std::shared_ptr<Edge> edge);
+
+protected:
+    std::unordered_set<std::shared_ptr<Node>> nodes;
+    std::unordered_set<std::shared_ptr<Edge>> edges;
+};
+
+class Node {
+public:
+    Node();
+
+    void addIncomingEdge(std::shared_ptr<Edge>);
+    void removeIncomingEdge(std::shared_ptr<Edge>);
+    void addOutgoingEdge(std::shared_ptr<Edge>);
+    void removeOutgoingEdge(std::shared_ptr<Edge>);
+
+    Point position;
+
+    virtual bool containsPoint(Point p) = 0;
+
+protected:
+    std::unordered_set<std::shared_ptr<Edge>> incomingEdges;
+    std::unordered_set<std::shared_ptr<Edge>> outgoingEdges;
+};
+
+class Edge {
+public:
+    Edge(std::shared_ptr<Node> begin, std::shared_ptr<Node> end) : begin(begin), end(end) { }
+
+protected:
+    std::shared_ptr<Node> begin;
+    std::shared_ptr<Node> end;
+};
+
+} // namespace oonalysis::graph
+
+#endif // GRAPH_GRAPH_HPP
